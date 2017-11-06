d9685ff0be46b4bec7fea8b2d60e1ab4bbfdfa6c
Carmen <carmenmok1697@gmail.com>
2017-11-06 14:29:36 -0600

    addToXML
    funcion de agregar nodos

--- src/java/xmlStuff.java
+++ src/java/xmlStuff.java
@@ -1,6 +1,7 @@
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,6 +13,10 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -45,26 +50,21 @@ public class xmlStuff {
     }
     
     //AGREGA ELEMENTO AL XML
-    public static void addToXML(String Root, String fileName, String content) throws TransformerException, ParserConfigurationException{
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+    public static void addToXML(String Root, String fileName, String content) throws TransformerException, ParserConfigurationException, SAXException, IOException{
+        String filepath = "C:\\Users\\carme\\Desktop\\"+Root+".xml";
+	DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	Document doc = docBuilder.parse(filepath);
         
-        Document document = documentBuilder.newDocument();
+        Node staff = doc.getElementsByTagName(Root).item(0);
         
-        Element element = document.createElement(Root);
-        document.appendChild(element);
-        
-        //Attr attr = document.createAttribute("ID");
-        //attr.setValue(id);
-        //element.setAttributeNode(attr);
-        
-        Element file = document.createElement(fileName);
-        file.appendChild(document.createTextNode(content));
-        element.appendChild(file);
-        
+        Element age = doc.createElement(fileName);
+	age.appendChild(doc.createTextNode(content));
+	staff.appendChild(age);
+                
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(document);
+        DOMSource source = new DOMSource(doc);
         
         String xmlfile = "C:\\Users\\carme\\Desktop\\"+Root+".xml";
         StreamResult streamResult = new StreamResult(new File(xmlfile));
@@ -72,59 +72,12 @@ public class xmlStuff {
         transformer.transform(source, streamResult);
     }
     
+    
     //MAIN
     public static void main(String[] args) throws Exception{
         xmlStuff xml = new xmlStuff();
-        xml.creatingXML("Directorio","1");
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        //xml.creatingXML("Directorio","1");
+        xml.addToXML("Directorio","file2","esto es un archivo");
         
-        Document document = documentBuilder.newDocument();
-        Element firstName = document.createElement("FirstName");
-        Element lastName = document.createElement("LastName");
-
-        firstName.appendChild(document.createTextNode("First Name"));
-        lastName.appendChild(document.createTextNode("Last Name"));
-
-        // create contact element
-        Element contact = document.createElement("contact");
-
-        // create attribute
-        Attr genderAttribute = document.createAttribute("gender");
-        genderAttribute.setValue("F");
-
-        // append attribute to contact element
-        contact.setAttributeNode(genderAttribute);
-        contact.appendChild(firstName);
-        contact.appendChild(lastName);
-        
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(document);
-        
-        String xmlfile = "C:\\Users\\carme\\Desktop\\"+"Directorio"+".xml";
-        StreamResult streamResult = new StreamResult(new File(xmlfile));
-        
-        transformer.transform(source, streamResult);
-        /**DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        
-        Document document = documentBuilder.newDocument();
-        
-        Element element = document.createElement("Directorio");
-        document.appendChild(element);
-        
-        Element file = document.createElement("file");
-        file.appendChild(document.createTextNode("content"));
-        element.appendChild(file);
-        
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(document);
-        
-        String xmlfile = "C:\\Users\\carme\\Desktop\\"+"Directorio"+".xml";
-        StreamResult streamResult = new StreamResult(new File(xmlfile));
-        
-        transformer.transform(source, streamResult);**/
     }
 }
