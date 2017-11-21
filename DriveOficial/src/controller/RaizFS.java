917634f658f870f417a61e4f7b31587047ffd541
Ariana B <bermudezarii@gmail.com>
2017-11-20 23:40:49 -0600

    Dir virtuales a reales

--- DriveOficial/src/controller/RaizFS.java
+++ DriveOficial/src/controller/RaizFS.java
@@ -50,7 +50,7 @@ public class RaizFS {
         Archivo archivo = new Archivo(nombreArchivo, extension, contenido); 
         consumido += contenido.length(); 
         if (limiteTamanio < consumido){
-            System.out.println("excede la cantidad de consumo de su drive");
+            System.out.println("Excede la cantidad de consumo de su drive");
             consumido -= contenido.length(); 
             return false; 
         }
@@ -68,9 +68,7 @@ public class RaizFS {
     public boolean copiarRV(){return false;}
     public boolean copiarVR(){return false;}
     public boolean copiarVV(){return false;}
-    public String relativo_a_absoluto(){
-        return "uqka"; 
-    }
+    
     public boolean eliminarArchivo(String nombre, String extension){
         Directorio dir = encontrarDirectorio(dirActual); 
         ArrayList<Archivo> archivos = dir.getArchivos();
@@ -96,9 +94,7 @@ public class RaizFS {
             for (Directorio dir1 : dirs) {
                 if (dir1.getNombre().equals(nombre)){
                     borrarEnHijos(dir1); 
-                    System.out.println("aqui?");
                     dir.quitarCarpeta(dir1);
-                    System.out.println("wat 2");
                     return true;
                 }
             }
@@ -110,12 +106,7 @@ public class RaizFS {
         @sumary: es navegar al directorio que me piden, y devuelvo el  
         directorio para que ya con eso haga lo que ocupa, copiar, mover, etc 
     */ 
-        String[] carpetasDeDirSolicitado = directorioSolicitado.split("\\/");
-        System.out.println("holi ");// son las carpetas de la direccion separadas 
-        for (String string : carpetasDeDirSolicitado) {
-            System.out.println(string);
-        }
-        System.out.println("holi ");
+        String[] carpetasDeDirSolicitado = directorioSolicitado.split("\\/"); // son las carpetas de la direccion separadas 
         Directorio temp = dir;                                                          // se situa en la pura raiz, con ese temp vamos a navegar 
         if(carpetasDeDirSolicitado.length == 1 && carpetasDeDirSolicitado[0].equals(dir.getNombre())){ 
             return dir;  
@@ -123,21 +114,17 @@ public class RaizFS {
         else{ 
             for(int i = 0; i < carpetasDeDirSolicitado.length; i++){
                 String string = carpetasDeDirSolicitado[i];
-                System.out.println("iteracion de string " + i);
                 ArrayList<Directorio> directorios = temp.getDirectorios();  
                 int indice = i; 
                 for (Directorio directorio : directorios) { 
                     if(directorio.getNombre().equals(string)){ 
-                        System.out.println("victory " + string );
                         //int indice = directorios.indexOf(directorio);  
-                        System.out.println("indice " + indice + " y el carp " + (carpetasDeDirSolicitado.length - 1));
                         if(indice == (carpetasDeDirSolicitado.length - 1)) 
                         { 
                             return directorio; 
                         } 
                         else{ 
                             temp = directorio;
-                            System.out.println(directorio);
                         }
                     } 
                 } 
@@ -150,7 +137,60 @@ public class RaizFS {
     public boolean moverCarpeta(){return false;}
     public boolean modificarArchivo(){return false;}
     public void verArchivo(){}
-
+    public String virtual_a_real(String dirBase, String direccionVirtual){
+        String[] virtualArray = direccionVirtual.split("\\/"); 
+        for (String virtualSegmento : virtualArray) {
+            if(virtualSegmento.equals("..")){
+                /*
+                    En este caso habria que buscar al papa con
+                ayuda de dirBase, y cuando ya lo encuentra se
+                sigue pusheando la dirVirtual normalmente
+                Se valida que la dirBase no sea el mismo nombre
+                del directorio base (dir) porque sino no tiene
+                a quien buscarle papa y seria un error 
+                */
+                
+                String[] dirBaseArray = dirBase.split("\\/"); 
+                dirBase = ""; // se formatea para que se realice una nueva dir sobre ella 
+                if(!dirBase.equals(dir.getNombre())){
+                    for (int i = 0; i < (dirBaseArray.length-1); i++){
+                        if(dirBase.equals("")){
+                            /*
+                                Se mete a dirBase como el inicio 
+                            de verdad sin / o cosas raras invalidas
+                            */
+                            dirBase += dirBaseArray[i]; 
+                        }
+                        else{
+                            /*
+                                Aqui ya se mete con los separadores
+                            correspondientes (: 
+                            */
+                            dirBase += "/" + dirBaseArray[i]; 
+                        }
+                    }
+                } 
+            }
+            else if(!virtualSegmento.equals(".")){
+                /*
+                    Basicamente si sale un punto(".") se ignora
+                y se le empieza a meter a dirBase la dirVirtual
+                entonces ya queda como Dios manda
+                */
+                dirBase += "/" + virtualSegmento; 
+            }
+        }
+        return dirBase; //va a tener la nueva dir 
+    }
+    public String verificacionVirtual_a_Real(String direccion){
+        String[] direccionSplit = direccion.split("\\/");
+        if (direccionSplit[0].equals(dir.getNombre())){
+            return direccion;
+        }
+        else{
+            return virtual_a_real(dirActual, direccion); 
+        }
+    }
     @Override
     public String toString() {
         return "RaizFS{" + "nombreRaiz=" + nombreRaiz + ", consumido=" + consumido + ", limiteTamanio=" + limiteTamanio + ", dir=" + dir + '}';
--- DriveOficial/src/driveoficial/DriveOficial.java
+++ DriveOficial/src/driveoficial/DriveOficial.java
@@ -5,8 +5,6 @@
  */
 package driveoficial;
 
-import model.Directorio;
-import model.Archivo; 
 import model.Usuario;
 
 /**
@@ -18,8 +16,7 @@ public class DriveOficial {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void prueba1(){
         Usuario user = new Usuario("bermudezari", "1234", 50000);
         user.getFileSystem().cambiarDirActual("D/Compartido");
         user.getFileSystem().crearDirectorio("CUAL"); 
@@ -28,7 +25,27 @@ public class DriveOficial {
         user.getFileSystem().cambiarDirActual("D");
         user.getFileSystem().eliminarDirectorio("Compartido");
         System.out.println(user.toString());
+    }
+    
+    public static void prueba2(){
+        Usuario user = new Usuario("bermudezari", "1234", 50000);
+        user.getFileSystem().cambiarDirActual("D/Compartido");
+        user.getFileSystem().crearDirectorio("CUAL"); 
+        user.getFileSystem().cambiarDirActual("D/Compartido/CUAL");
+        user.getFileSystem().crearArchivo("ari", "pdf", "salsa a la 1 am");
+        user.getFileSystem().crearDirectorio("Me miau"); 
+        user.getFileSystem().cambiarDirActual("D/Compartido");
+        user.getFileSystem().verificacionVirtual_a_Real("./CUAL"); 
+        user.getFileSystem().verificacionVirtual_a_Real("../"); 
+        user.getFileSystem().verificacionVirtual_a_Real("D/Compartido/CUAL"); 
+        System.out.println(user.toString());
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        prueba2(); 
         
     }
+    
+    
     
 }
